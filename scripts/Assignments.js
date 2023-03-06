131f27e10957c495edca21fe1641c8d90ec94981
guacajoely <guacajoely@gmail.com>
2023-03-06 15:56:03 -0500

    refactored JS to correct city names in assignments

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -4,14 +4,15 @@
 // 3. AssingmentHTML was missing += and therefore only outputting the final assignment
 
 
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const cities = getCities()
 
 
-// Function whose responsibility is to find the walker assigned to a pet
+// Function whose responsibility is to find the PET assigned to a walker
 const findWalker = (pet, allWalkers) => {
     let petWalker = null
 
@@ -24,6 +25,21 @@ const findWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+// Function whose responsibility is to find the CITY assigned to a walker
+const findCity = (walkerObject, cities) => {
+    let matchingCity = null
+
+    for (const city of cities) {
+        if (city.id === walkerObject.cityId) {
+            matchingCity = city.name
+        }
+    }
+
+    return matchingCity
+}
+
+
+// RUN THE TWO FUNCTIONS AND THROW IT ALL IN A STRING FOR EACH PET
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
@@ -31,11 +47,13 @@ export const Assignments = () => {
     for (const currentPet of pets) {
 
         const currentPetWalker = findWalker(currentPet, walkers)
+        const currentPetCity = findCity(currentPetWalker, cities)
+
 
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentPetCity}
             </li>
         `
     }
